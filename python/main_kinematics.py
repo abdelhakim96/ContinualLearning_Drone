a7b68fa2d317a4c3b8e1e3082f78700dd6a515ea
andriyukr <andriyukr@gmail.com>
2022-05-24 12:56:09 +0800

    Update main_kinematics.py

--- python/main_kinematics.py
+++ python/main_kinematics.py
@@ -14,7 +14,7 @@ from utils.show import show_plots, show_animation
 collect_data = False  # True - collect data, False - test performance
 controller = 2  # 0 - random, 1 - PID, 2 - DNN, 3 - inverse
 online_learning = True  # For DNN (controller = 2): True - enable online learning, False - disable online learning
-trajectory = 1  # 0 - random points, 1 - circular, 2 - 8-shaped, 3 - set-point, 4 - square-wave
+trajectory = 2  # 0 - random points, 1 - circular, 2 - 8-shaped, 3 - set-point, 4 - square-wave
 uncertainty = 1  # internal uncertainty: 0 - no uncertainty, 1 - all parameters double, -1 - all parameters half;
 # default = 1
 disturbance = -2  # external disturbance: 0 - no disturbance, >0 - positive disturbance, <0 - negative disturbance
